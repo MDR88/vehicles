a55ca8a6507289ab89c142ea9de35fd66187d40d
mdroberts74 <mikedroberts88@gmail.com>
2018-08-28 14:46:41 -0500

    Created IVehicle into it's own module.

--- Program.cs
+++ Program.cs
@@ -2,24 +2,6 @@
 using System.Linq;
 using System.Collections.Generic;
 
-
-public interface IVehicle
-{
-    int Wheels { get; set; }
-    int Doors { get; set; }
-    int PassengerCapacity { get; set; }
-    bool Winged { get; set; }
-    string TransmissionType { get; set; }
-    double EngineVolume { get; set; }
-    double MaxWaterSpeed { get; set; }
-    double MaxLandSpeed { get; set; }
-    double MaxAirSpeed { get; set; }
-    void Start();
-    void Stop();
-    void Drive();
-    void Fly();
-}
-
 public class JetSki : IVehicle
 {
     public int Wheels { get; set; }
@@ -51,6 +33,10 @@ public class JetSki : IVehicle
     {
         throw new NotImplementedException();
     }
+}
+
+public interface IVehicle
+{
 }
 
 public class Motorcycle : IVehicle
@@ -88,43 +74,42 @@ public class Motorcycle : IVehicle
 
 public class Cessna : IVehicle
 {
-    public int Wheels { get; set; } = 3;
-    public int Doors { get; set; } = 3;
-    public int PassengerCapacity { get; set; } = 113;
-    public bool Winged { get; set; } = true;
-    public string TransmissionType { get; set; } = "None";
-    public double EngineVolume { get; set; } = 31.1;
-    public double MaxWaterSpeed { get; set; }
-    public double MaxLandSpeed { get; set; }
-    public double MaxAirSpeed { get; set; } = 309.0;
+  public int Wheels { get; set; } = 3;
+  public int Doors { get; set; } = 3;
+  public int PassengerCapacity { get; set; } = 113;
+  public bool Winged { get; set; } = true;
+  public string TransmissionType { get; set; } = "None";
+  public double EngineVolume { get; set; } = 31.1;
+  public double MaxWaterSpeed { get; set; }
+  public double MaxLandSpeed { get; set; }
+  public double MaxAirSpeed { get; set; } = 309.0;
 
-    public void Drive()
-    {
-        throw new NotImplementedException();
-    }
+  public void Drive()
+  {
+    throw new NotImplementedException();
+  }
 
-    public void Fly()
-    {
-        Console.WriteLine("The Cessna effortlessly glides through the clouds like a gleaming god of the Sun");
-    }
+  public void Fly()
+  {
+    Console.WriteLine("The Cessna effortlessly glides through the clouds like a gleaming god of the Sun");
+  }
 
-    public void Start()
-    {
-        throw new NotImplementedException();
-    }
+  public void Start()
+  {
+    throw new NotImplementedException();
+  }
 
-    public void Stop()
-    {
-        throw new NotImplementedException();
-    }
+  public void Stop()
+  {
+    throw new NotImplementedException();
+  }
 }
 
 
 public class Program
 {
 
-    public static void Main()
-    {
+    public static void Main() {
 
         // Build a collection of all vehicles that fly
 
@@ -139,7 +124,7 @@ public class Program
 
 
         // Build a collection of all vehicles that operate on water
-
+        
         // With a single `foreach`, have each water vehicle Drive()
     }
 
